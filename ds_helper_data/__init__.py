1996aacd334e45ae9b9a038d1aff370f76c38925
JimKing100 <JimKing100@users.noreply.github.com>
2019-10-26 18:55:29 -0700

    added some proof of concept utility functions

---
+++ ds_helper_data/__init__.py
@@ -0,0 +1,11 @@
+"""
+ds_helper_data - a collection of datascience helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+# sample code
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
