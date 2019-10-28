b4cc1bac61d649f843df7ce0599612aeedd9a5bd
JimKing100 <JimKing100@users.noreply.github.com>
2019-10-28 10:08:48 -0700

    add NULLS to df_utils

--- ds_helper_data/df_utils.py
+++ ds_helper_data/df_utils.py
@@ -1,7 +1,16 @@
 """
-utiltiy functions for working with DataFrames
+utility functions for working with DataFrames
 """
 
 import pandas
 
 TEST_DF = pandas.DataFrame([1,2,3])
+
+def NULLS(df):
+  columns = list(df)
+  col_num = 0
+  print('Nulls by Column')
+
+  for i in columns:
+    print(columns[col_num], df[i].isna().sum())
+    col_num = col_num + 1
