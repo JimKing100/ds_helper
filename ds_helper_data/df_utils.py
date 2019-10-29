ea12f773c991d5b1153b2e35d695294c707e5638
JimKing100 <JimKing100@users.noreply.github.com>
2019-10-29 09:08:18 -0700

    bug fix

--- ds_helper_data/df_utils.py
+++ ds_helper_data/df_utils.py
@@ -2,7 +2,7 @@
 utility functions for working with DataFrames
 """
 
-import pandas as pd
+import pandas
 from datetime import datetime
 
 TEST_DF = pandas.DataFrame([1,2,3])
@@ -17,7 +17,7 @@ def nulls(df):
     col_num = col_num + 1
 
 def date_conv(df, col):
-  df[col] = pd.to_datetime(df[col], infer_datetime_format=True)
+  df[col] = pandas.to_datetime(df[col], infer_datetime_format=True)
   df['year'] = df[col].dt.year
   df['month'] = df[col].dt.month
   df['day'] = df[col].dt.day
